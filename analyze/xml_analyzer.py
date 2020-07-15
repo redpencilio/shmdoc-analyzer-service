545e9d75797dcfa2ce8f494bd3e305344150900f
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-07-15 16:15:00 +0200

    Analyzing longest list

--- analyze/xml_analyzer.py
+++ analyze/xml_analyzer.py
@@ -14,13 +14,20 @@ def xml_to_dataframe(input_file):
     # Flatten the nested dictionary
     normalized = pandas.json_normalize(data)
 
+    longest_list = []
     for row in normalized:
         # TODO: zoek de kolom met de meeste rijen, want daarover willen we waarschijnlijk praten met shmdoc
         #  cause what it currently does is just take the data from the first row
         subdata = normalized[row]
+        if subdata.size < 1:
+            continue
         el = subdata[0]
-        norm = pandas.json_normalize(el)
-        return norm
+        if not isinstance(el, list):
+            continue
+        if len(el) > len(longest_list):
+            longest_list = el
+    norm = pandas.json_normalize(longest_list)
+    return norm
     # normalized.to_csv("output.csv")
 
     # TODO: Should find all the lists with more than n rows (recursively)
