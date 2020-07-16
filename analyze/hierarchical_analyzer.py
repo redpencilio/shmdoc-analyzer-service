d65981530dbded47f50985f7f31100bbc4e98807
Josse Van Delm <josse.vandelm@gmail.com>
2020-07-16 09:58:49 +0200

    Refactor xml_analyzer to house JSON analysis as well

--- analyze/hierarchical_analyzer.py
+++ analyze/hierarchical_analyzer.py
@@ -5,6 +5,9 @@ import json
 
 
 def find_longest_list(data):
+    """
+    In a nested dictionary, find the level with the most key/value-pairs
+    """
     longest_list = data  # with length data
     for child in data:
         if not isinstance(child, dict):
@@ -15,14 +18,25 @@ def find_longest_list(data):
     return longest_list
 
 
-def xml_to_dataframe(input_file):
-    # Convert the xml file to a nested dictionary
-    with open(input_file) as fd:
-        doc = json.dumps(xmltodict.parse(fd.read()))
-
-    data = json.loads(doc)
-
-    # Flatten the nested dictionary
+def normalize_to_longest_list(unnormalized):
+    """
+    Find the longest list with find_longest_list() and normalize/flatten 
+    to this level.
+    """
     longest_list = find_longest_list(data)
     normalized = pandas.json_normalize(longest_list)
     return normalized
+
+
+def xml_to_dataframe(input_file):
+    # Convert the xml file to a nested dictionary
+    with open(input_file) as fd:
+    # Dump in string and then reconvert to not have orderedDicts
+        doc = json.dumps(xmltodict.parse(fd.read()))
+    data = json.loads(doc)
+    # Flatten the nested dictionary
+    normalized = normalize_to_longest_list(data)
+    return normalized
+
+
+
