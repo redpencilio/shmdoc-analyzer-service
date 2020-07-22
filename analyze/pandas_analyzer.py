ab72024638acfcc7116b1907975dad4fe0947be0
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-07-22 13:13:05 +0200

    Fix 0's and 1's recognized as bool

--- analyze/pandas_analyzer.py
+++ analyze/pandas_analyzer.py
@@ -15,18 +15,21 @@ except:
 
 from statistics import median
 
-typeUri = "http://www.w3.org/2001/XMLSchema#{type}"
-typeMap = {bool: typeUri.format(type="boolean"),
-           int: typeUri.format(type="integer"),
-           float: typeUri.format(type="float"),
-           str: typeUri.format(type="string"),
-           "datetime": typeUri.format(type="dateTime")}
+
+def is_int(el):
+    return isinstance(el, int)
 
 
-def type_url(type):
-    global typeMap
-    return typeMap[type]
+def is_bool(el):
+    return isinstance(el, bool) or (is_int(el) and (el == 0 or el == 1))
 
+
+def is_float(el):
+    return isinstance(el, float)
+
+
+def is_str(el):
+    return isinstance(el, str)
 
 def is_datetime(string):
     # Check whether there's any date / datetime somewhere in the string
@@ -38,6 +41,18 @@ def is_datetime(string):
     except TypeError:
         pass  # No idea why a TypeError is raised sometimes, but this solves the problem temporarily :-)
     return False
+
+typeUri = "http://www.w3.org/2001/XMLSchema#{type}"
+typeMap = {bool: (typeUri.format(type="boolean"), is_bool),
+           int: (typeUri.format(type="integer"), is_int),
+           float: (typeUri.format(type="float"), is_float),
+           str: (typeUri.format(type="string"), is_str),
+           "datetime": (typeUri.format(type="dateTime"), is_datetime)}
+
+
+def type_url(type):
+    global typeMap
+    return typeMap[type][0]
 
 
 # Given a string, what does the string probably contain?
@@ -141,9 +156,8 @@ def predict_type(column_data, col_obj):
         for type_ in typeMap:
             # Only check for trivial types (int, float, str, bool)
             # Other types (e.g. datetime) must be checked afterwards
-            if isinstance(type_, type):
-                if isinstance(element, type_):
-                    occurrences[type_url(type_)] += 1
+            if isinstance(type_, type) and typeMap[type_][1](element):
+                occurrences[type_url(type_)] += 1
 
     col_obj.missing_count = occurrences["empty"]
 
